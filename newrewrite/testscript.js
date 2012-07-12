5e256512313e1a3ebcf662d3783bbaccbeb578b6
Wes Kocher <kwierso@gmail.com>
2012-07-12 16:19:06 -0700

    More bettererer

--- newrewrite/testscript.js
+++ newrewrite/testscript.js
@@ -6,6 +6,7 @@ window.setTimeout(waitForAddon, 1000, true);
 function waitForAddon() {
   getAssigneeBreakdown();
   getBreakdown();
+  getPriorityBreakdown();
 
   document.body.removeAttribute("initial");
   document.body.removeChild(document.getElementById("initialFetch"));
@@ -48,7 +49,26 @@ function getAssigneeBreakdown() {
   request.send(null);
 }
 
+// initiate xhr to get breakdown data, pass it to d3.js
+function getPriorityBreakdown() {
+  var someURL = "https://api-dev.bugzilla.mozilla.org/latest/count?product=Add-on%20SDK&y_axis_field=priority&x_axis_field=status&status=NEW&status=ASSIGNED&status=UNCONFIRMED&status=REOPENED";
 
+  var request = new XMLHttpRequest();
+  request.open('GET', someURL, true);
+  request.setRequestHeader("Accept", "application/json");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onreadystatechange = function (aEvt) {
+    if (request.readyState == 4) {
+      if(request.status == 200) {
+        priorityBreakdownFixed(JSON.parse(request.response));
+      } else {
+        alert("Something with the request went wrong. Request status: " + request.status);
+        document.body.removeAttribute("activeRequests");
+      }
+    }
+  };
+  request.send(null);
+}
 
 // initiate xhr to get breakdown data, pass it to d3.js
 function getBreakdown() {
@@ -70,9 +90,6 @@ function getBreakdown() {
   };
   request.send(null);
 }
-
-
-
 
 function assigneeBreakdownFixed(data) {
   var color = d3.scale.category20b();
@@ -116,7 +133,8 @@ function assigneeBreakdownFixed(data) {
     header.setAttribute("index", head.childNodes.length);
     head.appendChild(header);
 
-    var totals;
+    var total;
+    var totalArray = [];
     // For every row added to the table, parse out the data and add a table cell for it
     for(var i=0;i<tablerows.length;i++) {
       // Get this row's data
@@ -134,10 +152,10 @@ function assigneeBreakdownFixed(data) {
         getUserName(yHeader, "https://api-dev.bugzilla.mozilla.org/latest/user/" + yHeads[i]);
       }
 
-      totals = 0;
+      total = 0;
       // For every item in the data set, create a row for the item
       for(j in thisRowData) {
-        totals = totals + thisRowData[j];
+        total = total + thisRowData[j];
         var cell = document.createElement("td");
         if(thisRowData[j] > 0) {
           cell.innerHTML = thisRowData[j];
@@ -151,8 +169,9 @@ function assigneeBreakdownFixed(data) {
       
       // Calculate totals
       var cell = document.createElement("td");
-      cell.innerHTML = totals;
-      if(totals > 0) {
+      cell.innerHTML = total;
+      totalArray.push(total);
+      if(total > 0) {
         tablerows[i].setAttribute("style", "background: " + color(i) + ";");
       }
       tablerows[i].appendChild(cell);
@@ -202,7 +221,6 @@ function assigneeBreakdownFixed(data) {
       }, false);
     }
   }
-
   document.getElementById("assigneeCounts").removeAttribute("notloaded");
   return data;
 }
@@ -226,7 +244,118 @@ function getUserName(header, userURI) {
 }
 
 
-// This is the incoming information for the bug breakdown by status and assignee
+// This is the incoming information for the bug breakdown by status and milestone
+function priorityBreakdownFixed(data) {
+    // These are the rows in the breakdown table
+    var tablerows = document.getElementById("priorityTable")
+                            .getElementsByTagName("tbody")[0]
+                            .getElementsByTagName("tr");
+    // This is the breakdown table's "thead" element
+    var head = document.getElementById("priorityTable")
+                       .getElementsByTagName("thead")[0];
+
+    var mstones = [];
+
+    // The various datapoints used in the table
+    var counts = data.data;
+    var xHeads = data.x_labels;
+    var yHeads = data.y_labels;
+
+    if(counts == "") {
+      document.getElementById("zarroBoogs").removeAttribute("unneeded");
+    } else {
+      // Use d3.js to add a row for each set of data
+      breakdowntrs = d3.select("#priorityTable").select("tbody").selectAll("tr")
+              .data(counts)
+            .enter().append("tr")
+            .attr("counts", function(d) { return d; });
+
+      // Put a blank th in the table for the empty top-left spot
+      var mstoneheader = document.createElement("th");
+      mstoneheader.innerHTML = ""
+      head.appendChild(mstoneheader);
+
+      // Add a header element for each column in the table
+      // XXX TODO Add sorting? (It's all numbers, should be easy...)
+      for(i in xHeads) {
+        var header = document.createElement("th");
+        header.innerHTML = xHeads[i];
+        header.setAttribute("index", head.childNodes.length);
+        
+        /*header.addEventListener("click", function(evt) {
+          var bugtable = document.getElementById("bugtable");
+          bugtable.setAttribute("filtermilestone", evt.target.innerHTML);
+          bugtable.removeAttribute("filterstatus");
+          document.getElementById("breakdownTable").removeAttribute("selectedstatus");
+          document.getElementById("piemilestone").removeAttribute("selectedstatus");
+          document.getElementById("breakdownTable").removeAttribute("selectedStatus");
+          document.getElementById("piemilestone").removeAttribute("selectedStatus");
+        }, false);*/
+        
+        head.appendChild(header);
+      }
+
+      // For every row added to the table, parse out the data and add a table cell for it
+      for(var i=0;i<tablerows.length;i++) {
+        // Get this row's data
+        var thisRowData = JSON.parse("[" + tablerows[i].getAttribute("counts") + "]");
+
+        // Add the first column from the yHeads dataset
+        var yHeader = document.createElement("td");
+        yHeader.innerHTML = yHeads[i];
+        yHeader.className = "first";
+        tablerows[i].appendChild(yHeader);
+        yHeader.addEventListener("click", function(evt) {
+          var priority = evt.target.innerHTML;
+          var thisURL = "https://bugzilla.mozilla.org/buglist.cgi?list_id=3688684;query_format=advanced;priority=&&PRIORITY&&;bug_status=UNCONFIRMED;bug_status=NEW;bug_status=ASSIGNED;bug_status=REOPENED;product=Add-on%20SDK";
+          thisURL = thisURL.replace("&&PRIORITY&&", priority);
+          window.open(thisURL);
+        }, false);
+
+        // For every item in the data set, create a row for the item
+        for(j in thisRowData) {
+          var cell = document.createElement("td");
+          cell.innerHTML = thisRowData[j];
+          cell.addEventListener("click", function(evt) {
+            if(evt.target.innerHTML == "0") {
+              return;
+            }
+            var index = Array.prototype.indexOf.call(evt.target.parentNode.childNodes, evt.target);
+            var status = evt.target.parentNode.parentNode.parentNode.getElementsByTagName("th")[index].innerHTML;
+            var priority = evt.target.parentNode.firstChild.innerHTML;
+            var thisURL = "https://bugzilla.mozilla.org/buglist.cgi?list_id=3688684;query_format=advanced;priority=&&PRIORITY&&;bug_status=&&STATUS&&;product=Add-on%20SDK";
+            thisURL = thisURL.replace("&&STATUS&&", status);
+            thisURL = thisURL.replace("&&PRIORITY&&", priority);
+            window.open(thisURL);
+          }, false);
+          tablerows[i].appendChild(cell);
+        }
+      }
+
+      // Draw a pie chart of the priority breakdown, sum all milestones if all are shown
+      var priority = [];
+      if(typeof counts[0] == "number") {
+        for(i in counts) {
+          priority.push(counts[i])
+        }
+      } else {
+        for(i in counts) {
+          var thisSum = 0;
+          for(j in counts[i]) {
+            thisSum = thisSum + counts[i][j];
+          }
+          priority.push(thisSum);
+        }
+      }
+      pie(priority, yHeads, "priority");
+      document.getElementById("priority").removeAttribute("notloaded");
+    }
+
+    return data;
+}
+
+
+// This is the incoming information for the bug breakdown by status and milestone
 function breakdownFixed(data) {
     // These are the rows in the breakdown table
     var tablerows = document.getElementById("breakdownTable")
@@ -269,9 +398,9 @@ function breakdownFixed(data) {
           bugtable.setAttribute("filtermilestone", evt.target.innerHTML);
           bugtable.removeAttribute("filterstatus");
           document.getElementById("breakdownTable").removeAttribute("selectedstatus");
-          document.getElementById("pie").removeAttribute("selectedstatus");
+          document.getElementById("piemilestone").removeAttribute("selectedstatus");
           document.getElementById("breakdownTable").removeAttribute("selectedStatus");
-          document.getElementById("pie").removeAttribute("selectedStatus");
+          document.getElementById("piemilestone").removeAttribute("selectedStatus");
         }, false);
         
         head.appendChild(header);
@@ -289,15 +418,9 @@ function breakdownFixed(data) {
         tablerows[i].appendChild(yHeader);
         yHeader.addEventListener("click", function(evt) {
           var target = evt.target.innerHTML;
-          var arcs = document.getElementById("pie").getElementsByTagName("g");
-          for(i in arcs) {
-            if(arcs[i].getAttribute("status") == target) {
-                var evt = document.createEvent("MouseEvents");
-                evt.initMouseEvent("click", true, true, window,
-                0, 0, 0, 0, 0, false, false, false, false, 0, null);
-                arcs[i].dispatchEvent(evt);
-            }
-          }
+          var thisURL = "https://bugzilla.mozilla.org/buglist.cgi?list_id=3688684;query_format=advanced;bug_status=&&STATUS&&;product=Add-on%20SDK";
+          thisURL = thisURL.replace("&&STATUS&&", target);
+          window.open(thisURL);
         }, false);
 
         // For every item in the data set, create a row for the item
@@ -305,6 +428,18 @@ function breakdownFixed(data) {
           var cell = document.createElement("td");
           cell.innerHTML = thisRowData[j];
           tablerows[i].appendChild(cell);
+          cell.addEventListener("click", function(evt) {
+            if(evt.target.innerHTML == "0") {
+              return;
+            }
+            var index = Array.prototype.indexOf.call(evt.target.parentNode.childNodes, evt.target);
+            var milestone = evt.target.parentNode.parentNode.parentNode.getElementsByTagName("th")[index].innerHTML;
+            var status = evt.target.parentNode.firstChild.innerHTML;
+            var thisURL = "https://bugzilla.mozilla.org/buglist.cgi?list_id=3688684;query_format=advanced;bug_status=&&STATUS&&;target_milestone=&&MILESTONE&&;product=Add-on%20SDK";
+            thisURL = thisURL.replace("&&MILESTONE&&", milestone);
+            thisURL = thisURL.replace("&&STATUS&&", status);
+            window.open(thisURL);
+          }, false);
         }
       }
 
@@ -342,13 +477,13 @@ function pie(data, status, target) {
 
     if(target == "milestone") {
       myTarget = "#breakdown";
-    } else {
-      myTarget = "#assigneeCounts";
+    } else if(target == "priority") {
+      myTarget = "#priority";
     }
     var vis = d3.select(myTarget)
       .append("svg:svg")
         .data([data])
-        .attr("id", "pie")
+        .attr("id", "pie" + target)
         .attr("width", w)
         .attr("height", h);
 
@@ -369,32 +504,39 @@ function pie(data, status, target) {
         .attr("display", function(d) { return d.value > .15 ? null : "none"; })
         .text(function(d, i) { return d.value; });
 
-    // Give the breakdown table the colors from the pie chart
-    var tablerows = document.getElementById("breakdownTable")
+    var tablerows, paths, pieArcs;
+    if(target == "milestone") {
+      tablerows = document.getElementById("breakdownTable")
                             .getElementsByTagName("tbody")[0]
                             .getElementsByTagName("tr");
-    var paths = document.getElementById("pie").getElementsByTagName("path");
+      paths = document.getElementById("piemilestone").getElementsByTagName("path");
+      pieArcs = document.getElementById("piemilestone").getElementsByTagName("g");
+    } else if(target == "priority") {
+      tablerows = document.getElementById("priorityTable")
+                            .getElementsByTagName("tbody")[0]
+                            .getElementsByTagName("tr");
+      paths = document.getElementById("piepriority").getElementsByTagName("path");
+      pieArcs = document.getElementById("piepriority").getElementsByTagName("g");
+    }
+    
     for(var i=0;i<tablerows.length;i++) {
         tablerows[i].style.background = paths[i].getAttribute("fill");
     }
 
-    if(target == "milestone") {
-      var pieArcs = document.getElementById("pie").getElementsByTagName("g");
-      for(var i=0;i<pieArcs.length;i++) {
-        pieArcs[i].setAttribute("status", status[i]);
-        pieArcs[i].addEventListener("click", function(e) {
-          pieClick(e.originalTarget);
-        }, false);
-      }
+    for(var i=0;i<pieArcs.length;i++) {
+      pieArcs[i].setAttribute("status", status[i]);
+      pieArcs[i].addEventListener("click", function(e) {
+        pieClick(e.originalTarget, target);
+      }, false);
     }
 }
 
 // Click handler for the pie chart
-function pieClick(tgt) {
+function pieClick(tgt, type) {
     while(tgt.nodeName != "g") {
       tgt = tgt.parentNode;
     }
-	
+
     var pie = tgt.parentNode;
     pie.setAttribute("selectedStatus", "true");
 
@@ -410,7 +552,12 @@ function pieClick(tgt) {
     var stat = tgt.getAttribute("status");
     tgt.setAttribute("selectedStatus", "true");
 
-    var bdTable = document.getElementById("breakdownTable");
+    var bdTable;
+    if(type == "milestone") {
+      bdTable = document.getElementById("breakdownTable");
+    } else if(type == "priority") {
+      bdTable = document.getElementById("priorityTable");
+    }
     var bdTableRows = bdTable.getElementsByTagName("tr");
     bdTable.setAttribute("selectedStatus", "true");
     for(var i=0;i<bdTableRows.length;i++) {
@@ -418,7 +565,4 @@ function pieClick(tgt) {
     }
     bdTableRows[arcIndex].setAttribute("selectedStatus", "true");
 
-    var bugtable = document.getElementById("bugtable");
-    bugtable.setAttribute("filterStatus", stat);
-    bugtable.removeAttribute("filtermilestone");
 }
