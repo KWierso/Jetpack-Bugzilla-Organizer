badea4cd87ebeb3f2c4ce967d188cdaaf501e478
Wes Kocher <kwierso@gmail.com>
2012-07-16 13:26:54 -0700

    Old bugs

--- newrewrite/testscript.js
+++ newrewrite/testscript.js
@@ -19,9 +19,14 @@ switch(query) {
   case "status":
     document.body.setAttribute("status", "true");
     break;
+
+  case "old":
+    document.body.setAttribute("old", "true");
+    break;
     
   default:
     document.getElementById("triageBreakdown").setAttribute("showing", "false");  
+    document.getElementById("oldBugs").setAttribute("showing", "false");  
 } 
 
 window.setTimeout(waitForAddon, 1000, true); 
@@ -31,6 +36,8 @@ function waitForAddon() {
   getBreakdown();
   getPriorityBreakdown();
   getTriageList();
+  getOldList();
+
   addToggles();
 
   document.body.removeAttribute("initial");
@@ -54,6 +61,78 @@ function addToggles() {
       }, false);
     }
   }
+}
+
+
+// initiate xhr to get old bug data, pass it to d3.js
+function getOldList() {
+  var someURL = "https://api-dev.bugzilla.mozilla.org/latest/bug?" +
+                "product=Add-on%20SDK&resolution=---&changed_before=1mon" +
+                "&include_fields=id,assigned_to,summary";
+
+  var request = new XMLHttpRequest();
+  request.open('GET', someURL, true);
+  request.setRequestHeader("Accept", "application/json");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onreadystatechange = function (aEvt) {
+    if (request.readyState == 4) {
+      if(request.status == 200) {
+        //assigneeBreakdownFixed(JSON.parse(request.response));
+        parseOldList(JSON.parse(request.response).bugs);
+      } else {
+        alert("Something with the request went wrong. Request status: " + request.status);
+        document.body.removeAttribute("activeRequests");
+      }
+    }
+  };
+  request.send(null);
+}
+
+function parseOldList(bugs) {
+  var color = d3.scale.category20();
+
+  var oldBugTable = document.getElementById("oldBugTable");
+  var thead = oldBugTable.getElementsByTagName("thead")[0];
+  var tbody = oldBugTable.getElementsByTagName("tbody")[0];
+
+  var total = 0;
+
+  bugs = bugs.sort(function(a,b) { return a.id > b.id; });
+  
+  var headers = ["id", "assigned_to", "summary"];
+  for(i in headers) {
+    var header = document.createElement("th");
+    header.innerHTML = headers[i];
+    thead.appendChild(header);
+  }
+  for(i in bugs) {
+    var bugRow = document.createElement("tr");
+    for(j in headers) {
+      var bugCell = document.createElement("td");
+      bugCell.textContent = bugs[i][headers[j]];
+      if(headers[j] == "summary") {
+        bugCell.setAttribute("title", bugs[i][headers[j]]);
+      }
+      if(headers[j] == "assigned_to") {
+        bugCell.textContent = bugs[i][headers[j]].name ? bugs[i][headers[j]].name : "";
+      }
+      bugCell.setAttribute("style", "background: " + color(i) + ";");
+      bugRow.appendChild(bugCell);
+    }
+    bugRow.addEventListener("click", function(evt) {
+      var tgt = evt.target;
+      while(tgt.tagName != "TR") {
+        tgt = tgt.parentNode;
+      }
+      tgt = tgt.firstChild.textContent;
+      window.open("https://bugzilla.mozilla.org/show_bug.cgi?id=" + tgt);
+    }, false);
+    
+    tbody.appendChild(bugRow);
+    total = total + 1;
+  }
+  document.getElementById("oldTotalCount").firstElementChild.textContent = total;
+  document.getElementById("oldBugs").removeAttribute("notloaded");
 }
 
 // initiate xhr to get triage data, pass it to d3.js
